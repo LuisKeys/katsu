3dba7f531c1c28a1f8e4eb5f87d56bfc40aea93f
LuisKeys <luis.paradela@gmail.com>
2024-01-30 12:08:22 -0300

    Simple testing listener

--- app.js
+++ app.js
@@ -1,16 +1,60 @@
-// Require the Bolt package (github.com/slackapi/bolt)
+// AskMe App POC
+
+const express = require('express');
+const app = express();
+const port = 80;
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
+
+/*
 const { App } = require("@slack/bolt");
-require('dotenv').config()
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
+// Listen for a slash command invocation
+app.command('/helloworld', async ({ ack, payload, context }) => {
+  // Acknowledge the command request
+  ack();
 
-(async () => {
-  // Start your app
-  //await app.start(process.env.PORT || 3000);
-
-  console.log('⚡️ Bolt app is running!');
-})();
+  try {
+    const result = await app.client.chat.postMessage({
+      token: context.botToken,
+      // Channel to send message to
+      channel: payload.channel_id,
+      // Include a button in the message (or whatever blocks you want!)
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: 'Go ahead. Click it.'
+          },
+          accessory: {
+            type: 'button',
+            text: {
+              type: 'plain_text',
+              text: 'Click me!'
+            },
+            action_id: 'button_abc'
+          }
+        }
+      ],
+      // Text in the notification
+      text: 'Message from Test App'
+    });
+    console.log(result);
+  }
+  catch (error) {
+    console.error(error);
+  }
+});
+*/
