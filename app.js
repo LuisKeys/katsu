8e0d2bc155f72546b2db3c6f39216a70627b5e5c
LuisKeys <luis.paradela@gmail.com>
2024-04-02 10:37:02 -0300

    Slack ready version

--- app.js
+++ app.js
@@ -15,85 +15,64 @@
 
   openai = new openAI();
 
-  // Test the promptHandler
-  const test = async () => {
-  //let prompt = "help link";  
-  let prompt = "List the roles where employee 'geronimo' works. List the customer name and the role.";
-  const result = await promptHandler.promptHandler(prompt, false);
-  const output = resultObject.render(result);
-  console.log(output);
-  // prompt = "Export to an excel file.";  
-  // await promptHandler.promptHandler(prompt, true);
-  }
+  // // Test the promptHandler
+  // const test = async () => {
+  // //let prompt = "help link";  
+  // let prompt = "List the roles where employee 'geronimo' works. List the customer name and the role.";
+  // const result = await promptHandler.promptHandler(prompt, false);
+  // const output = resultObject.render(result);
+  // console.log(output);
+  // // prompt = "Export to an excel file.";  
+  // // await promptHandler.promptHandler(prompt, true);
+  // }
 
-  test();
+  // test();
 
-  // // Bolt app Initialization
-  // const app = new App({
-  //   token: process.env.SLACK_BOT_TOKEN,
-  //   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  // });
+  // Bolt app Initialization
+  const app = new App({
+    token: process.env.SLACK_BOT_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+  });
 
-  // //listening for slash command invocation
-  // app.command("/askme", async ({ ack, payload, context }) => {
-  //   // Acknowledge the command request
-  //   ack();
+  //listening for slash command invocation
+  app.command("/askme", async ({ ack, payload, context }) => {
+    // Acknowledge the command request
+    ack();
 
-  //   const prompt = payload.text;
+    const prompt = payload.text;
 
-  //   try {
-  //     const sql = await nl2sql.generateSQL(openai, openAIAPI, prompt);
-    
-  //     console.log(sql);
-  //     const response = await sf_api.getData(sql);    
-  //     let output = prompt + ':\n';
+    try {
+      let output = prompt + ':\n';
+      const response = await promptHandler.promptHandler(prompt, false);
+      output += resultObject.render(response);
+
+      // Walk through response elements and concatenate them in the output string
+      await app.client.chat.postMessage({
+        token: context.botToken,
+        // Channel to send message to
+        channel: payload.channel_id,
+        // Include a button in the message (or whatever blocks you want!)
+        blocks: [
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: output, // Output message
+            },
+          },
+        ],
+        // Text in the notification
+        text: "Message from KATSU",
+      });
       
+    } catch (error) {
+      console.error(error);
+    }
+  });
 
-  //     // Walk through response elements and concatenate them in the output string
-  //     const linesLimit = 50;
-  //     let lineCounter = 0;
-  //     let totalLines = 0;
-  //     for(let i = 0; i < response.length; i++) {
-  //       const element = response[i];
-  //       lineCounter++;
-  //       totalLines++;
-  //       output += element + '\n';    
-          
-  //       if(lineCounter >= linesLimit || response.length === totalLines) {
-  //         const result = await app.client.chat.postMessage({
-  //           token: context.botToken,
-  //           // Channel to send message to
-  //           channel: payload.channel_id,
-  //           // Include a button in the message (or whatever blocks you want!)
-  //           blocks: [
-  //             {
-  //               type: "section",
-  //               text: {
-  //                 type: "mrkdwn",
-  //                 text: output, // Output message
-  //               },
-  //             },
-  //           ],
-  //           // Text in the notification
-  //           text: "Message from Test App",
-  //         });
+  (async () => {
+    // Start your app
+    await app.start(3000);
 
-  //         // Reset the output and counter
-  //         lineCounter = 0;
-  //         output = '';
-  //       }
-        
-  //     };    
-
-  //     //console.log(result);    
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // });
-
-  // (async () => {
-  //   // Start your app
-  //   await app.start(3000);
-
-  //   console.log("⚡ Bolt app is running on port 3000");
-  // })();
+    console.log("⚡ Bolt app is running on port 3000");
+  })();
