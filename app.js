f248b91c9c95fa16b2e69e8925f7083210b66a2c
LuisKeys <luis.paradela@gmail.com>
2024-04-11 15:16:50 -0300

    messaging refactor

--- app.js
+++ app.js
@@ -1,166 +1,201 @@
-  /**
-   * This is the main file for the AskMe App POC.
-   * It initializes the Slack Bolt app, authenticates with DB,
-   * and listens for a slash command invocation to send a test message.
-   * It also has a test() method to test the nl2sql and openAIAPI modules local.
-   */
+/**
+ * This is the main file for the AskMe App POC.
+ * It initializes the Slack Bolt app, authenticates with DB,
+ * and listens for a slash command invocation to send a test message.
+ * It also has a test() method to test the nl2sql and openAIAPI modules local.
+ */
 
-  // Required External Modules
-  const { App } = require("@slack/bolt");
-  const openAI = require("openai");
-  const fs = require("fs");
-  const promptHandler = require("./prompt_handler");
-  const resultObject = require("./src/prompts/result_object");
-  const getMember = require("./src/members/get_member");
-  const answerPhrase = require("./src/prompts/answer_phrases");
-  require("dotenv").config();
+// Required External Modules
+const { App } = require("@slack/bolt");
+const openAI = require("openai");
+const fs = require("fs");
+const promptHandler = require("./prompt_handler");
+const resultObject = require("./src/prompts/result_object");
+const getMember = require("./src/members/get_member");
+const answerPhrase = require("./src/prompts/answer_phrases");
+require("dotenv").config();
 
-  openai = new openAI();
+openai = new openAI();
 
-  let users
+let users;
 
-  const isDebug = process.env.KATSU_DEBUG == 'true';
+const isDebug = process.env.KATSU_DEBUG == "true";
 
-  if (isDebug) {
-    console.log("Debug mode is on.");
+if (isDebug) {
+  console.log("Debug mode is on.");
 
-    // Test the promptHandler
-    const test = async () => {
-      const memberId = await getMember.getMemberId("luis@accelone.com");
-      const isValid = memberId != -1;
-      if (!isValid) {
-        console.log(
-          "You are not a registered user. Please contact the administrator to register."
-        );
-        return;
-      } else {
-        const prompt = "list all the active engagements.";
-        // const prompt = "help";
-        let result = await promptHandler.promptHandler(prompt, memberId, false, "luis");
+  // Test the promptHandler
+  const test = async () => {
+    const memberId = await getMember.getMemberId("luis@accelone.com");
+    const isValid = memberId != -1;
+    if (!isValid) {
+      console.log(
+        "You are not a registered user. Please contact the administrator to register."
+      );
+      return;
+    } else {
+      const prompt = "list all the active engagements.";
+      // const prompt = "help";
+      let result = await promptHandler.promptHandler(
+        prompt,
+        memberId,
+        false,
+        "luis"
+      );
 
-        let hey = answerPhrase.getAnswerPhrase("Luis") + "!\n";
-        hey += prompt + "\n";
-        let output = resultObject.render(result);
-        output = hey + "```" + output + "```";
-
-        console.log(output);
-      }
-    };
-
-    test();
-
-  } else {
-    // Bolt app Initialization
-    const app = new App({
-      token: process.env.SLACK_BOT_TOKEN,
-      signingSecret: process.env.SLACK_SIGNING_SECRET,
-    });
-
-    const loadUsers = async () => {
-      users = await app.client.users.list();
-    };
-
-    loadUsers();
-
-    const getAnswer = async (prompt, profile) => {
-      const memberId = await getMember.getMemberId(profile.email);
-      const isValid = memberId != -1;
-
-      if (!isValid) {
-        return "You are not a registered user. Please contact the administrator to register.";
-      }
-
-      const response = await promptHandler.promptHandler(prompt, memberId, false, profile.first_name);
-      let hey = answerPhrase.getAnswerPhrase(profile.first_name) + "!\n";
+      let hey = answerPhrase.getAnswerPhrase("Luis") + "!\n";
       hey += prompt + "\n";
-      let output = resultObject.render(response);
+      let output = resultObject.render(result);
       output = hey + "```" + output + "```";
 
-      return output;
-    };
+      console.log(output);
+    }
+  };
 
-    // Listening for a message event
-    app.message("katsu", async ({ message, say }) => {
-      try {
-        let prompt = message.text.replace("katsu", "");
+  test();
+} else {
+  // Bolt app Initialization
+  const app = new App({
+    token: process.env.SLACK_BOT_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+  });
 
-        const profile = users.members.filter(
-          (member) => member.id === message.user
-        )[0].profile;
+  // Load workspace users
+  const loadUsers = async () => {
+    users = await app.client.users.list();
+  };
 
-        const output = await getAnswer(prompt, profile);
-        await say(output);
-      } catch (error) {
-        console.error(error);
-      }
-    });
+  loadUsers();
 
-    //listening for slash command invocation
-    app.command("/katsu", async ({ ack, payload, context }) => {
-      // Acknowledge the command request
-      ack();
+  /**
+   * Retrieves the answer based on the given prompt and user profile.
+   * @param {string} prompt - The prompt to be answered.
+   * @param {object} profile - The user's profile object.
+   * @returns {Promise<string>} The answer to the prompt.
+   */
+  const getAnswer = async (prompt, profile) => {
+    const memberId = await getMember.getMemberId(profile.email);
+    const isValid = memberId != -1;
 
-      const prompt = payload.text;
+    if (!isValid) {
+      return "You are not a registered user. Please contact the administrator to register.";
+    }
 
-      try {
-        const profile = users.members.filter(
-          (member) => member.id === payload.user_id
-        )[0].profile;
-        const output = await getAnswer(prompt, profile);
+    const response = await promptHandler.promptHandler(
+      prompt,
+      memberId,
+      false,
+      profile.first_name
+    );
+    let hey = answerPhrase.getAnswerPhrase(profile.first_name) + "!\n";
+    hey += prompt + "\n";
+    let output = resultObject.render(response);
+    output = hey + "```" + output + "```";
 
-        // Walk through response elements and concatenate them in the output string
-        await app.client.chat.postMessage({
-          token: context.botToken,
-          // Channel to send message to
-          channel: payload.channel_id,
-          // Include a button in the message (or whatever blocks you want!)
-          blocks: [
+    return output;
+  };
+
+  // Listening for a message event
+  app.message("katsu", async ({ message, say }) => {
+    try {
+      let prompt = message.text.replace("katsu", "");
+
+      const profile = users.members.filter(
+        (member) => member.id === message.user
+      )[0].profile;
+
+      const output = await getAnswer(prompt, profile);
+      await say(output);
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+  //Listening for slash command invocation
+  app.command("/katsu", async ({ ack, payload, context }) => {
+    // Acknowledge the command request
+    ack();
+
+    const prompt = payload.text;
+
+    try {
+      const profile = users.members.filter(
+        (member) => member.id === payload.user_id
+      )[0].profile;
+      const output = await getAnswer(prompt, profile);
+
+      await sendMessage(context.botToken, payload.channel_id, output);
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+  // Listening for the export excel action
+  app.action(
+    { action_id: "exportToExcel", block_id: "exportToExcel" },
+    async ({ body, client, ack, logger }) => {
+      await ack();
+
+      const profile = users.members.filter(
+        (member) => member.id === body.user.id
+      )[0].profile;
+
+      const prompt = "export to excel";
+      const output = await getAnswer(prompt, profile);
+
+      await sendMessage(body.token, body.container.container, output);
+    }
+  );
+
+  /**
+   * Sends a message to a specified channel using the Slack API.
+   *
+   * @param {string} token - The Slack API token.
+   * @param {string} channelId - The ID of the channel to send the message to.
+   * @param {string} output - The message content to send.
+   * @returns {Promise<void>} - A promise that resolves when the message is sent successfully.
+   */
+  const sendMessage = async (token, channelId, output) => {
+    await app.client.chat.postMessage({
+      token: token,
+      channel: channelId,
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: output, // Output message
+          },
+        },
+        {
+          type: "divider",
+        },
+        {
+          type: "actions",
+          block_id: "exportToExcel",
+          elements: [
             {
-              type: "section",
+              type: "button",
               text: {
-                type: "mrkdwn",
-                text: output, // Output message
-                // Added backticks to format the output as multine code
+                type: "plain_text",
+                text: "Export to Excel",
+                emoji: true,
               },
+              value: "exportToExcel",
+              action_id: "exportToExcel",
             },
-            {
-              type: "divider",
-            },
-            {
-              "type": "actions",
-              "elements": [
-                  {
-                      "type": "button",
-                      "text": {
-                          "type": "plain_text",
-                          "text": "Export to Excel",
-                          "emoji": true
-                      },
-                      "value": "exportToExcel",
-                      "action_id": "exportToExcel"
-                  }            
-            ]             
-          }            
           ],
-          // Text in the notification
-          text: "Message from KATSU",
-        });
-      } catch (error) {
-        console.error(error);
-      }
+        },
+      ],
+      // Text in the notification
+      text: "Message from KATSU",
     });
+  };
 
-// Export  to Excel handler
-app.action({ action_id: 'exportToExcel', block_id: 'assign_ticket' },
-async ({ body, client, ack, logger }) => {
-  await ack();
-  console.log(body);
-});
+  (async () => {
+    // Start your app
+    await app.start(3000);
 
-    (async () => {
-      // Start your app
-      await app.start(3000);
-
-      console.log("⚡ Bolt app is running on port 3000");
-    })();
-  }
+    console.log("⚡ Bolt app is running on port 3000");
+  })();
+}
