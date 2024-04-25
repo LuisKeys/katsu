b5c8d799c7ecf166905a258cd06b8c7c7ec2cf8d
LuisKeys <luis.paradela@gmail.com>
2024-04-25 09:57:04 -0300

    foxed demo mode for no answers

--- src/demo/demo_data.js
+++ src/demo/demo_data.js
@@ -7,89 +7,95 @@ const companyData = require("./company_data");
  * @returns {Array<Array<string>>} - The modified table data.
  */
 const replaceDemoValues = (result, entityName) => {
-  const fieldNames = result.fields;
+  if (result) {
+    const fieldNames = result.fields;
 
-  const lastNameIndex = fieldNames.findIndex(
-    (field) => field.name === "lastname"
-  );
-  const firstNameIndex = fieldNames.findIndex(
-    (field) => field.name === "firstname"
-  );
-  const emailIndex = fieldNames.findIndex((field) => field.name === "email");
-  const phoneIndex = fieldNames.findIndex((field) => field.name.indexOf("phone") > -1);
-  const fullNameIndex = fieldNames.findIndex(
-    (field) => field.name === "full_name"
-  );
-  const nameIndex = fieldNames.findIndex((field) => field.name === "name");
-  const customerNameIndex = fieldNames.findIndex(
-    (field) => field.name === "customer_name"
-  );
+    const lastNameIndex = fieldNames.findIndex(
+      (field) => field.name === "lastname"
+    );
+    const firstNameIndex = fieldNames.findIndex(
+      (field) => field.name === "firstname"
+    );
+    const emailIndex = fieldNames.findIndex((field) => field.name === "email");
+    const phoneIndex = fieldNames.findIndex(
+      (field) => field.name.indexOf("phone") > -1
+    );
+    const fullNameIndex = fieldNames.findIndex(
+      (field) => field.name === "full_name"
+    );
+    const nameIndex = fieldNames.findIndex((field) => field.name === "name");
+    const customerNameIndex = fieldNames.findIndex(
+      (field) => field.name === "customer_name"
+    );
 
-  let amountFields = [];
-  for(let i = 0; i < fieldNames.length; i++) {
-    if(fieldNames[i].name.indexOf("amount") > -1) {
-      amountFields.push(fieldNames[i].name);
+    let amountFields = [];
+    for (let i = 0; i < fieldNames.length; i++) {
+      if (fieldNames[i].name.indexOf("amount") > -1) {
+        amountFields.push(fieldNames[i].name);
+      }
     }
+
+    for (let i = 0; i < result.rows.length; i++) {
+      const lastName = peopleData.getLastName();
+      const firstName = peopleData.getFirstName();
+      const companyName = companyData.getCompanyName();
+      const email = companyData.getEmail(firstName, lastName, companyName);
+      const phone = peopleData.getPhone();
+      const amount = companyData.getAmount();
+
+      if (lastNameIndex !== -1) {
+        result.rows[i][fieldNames[lastNameIndex].name] = lastName;
+      }
+
+      if (firstNameIndex !== -1) {
+        result.rows[i][fieldNames[firstNameIndex].name] = firstName;
+      }
+
+      if (emailIndex !== -1) {
+        result.rows[i][fieldNames[emailIndex].name] = email;
+      }
+
+      if (phoneIndex !== -1) {
+        result.rows[i][fieldNames[phoneIndex].name] = phone;
+      }
+
+      if (fullNameIndex !== -1) {
+        result.rows[i][fieldNames[fullNameIndex].name] =
+          firstName + " " + lastName;
+      }
+
+      if (nameIndex !== -1) {
+        let name = firstName + " " + lastName;
+
+        if (entityName === "company") {
+          name = companyName;
+        }
+
+        if (entityName === "engagement") {
+          name = companyName;
+        }
+
+        result.rows[i][fieldNames[nameIndex].name] = name;
+      }
+
+      if (customerNameIndex !== -1) {
+        if (entityName === "project_role") {
+          result.rows[i][fieldNames[customerNameIndex].name] = companyName;
+        }
+      }
+
+      if (amountFields.length > 0) {
+        for (let j = 0; j < amountFields.length; j++) {
+          result.rows[i][amountFields[j]] = amount;
+        }
+      }
+    }
+
+    return result;
+  } else {
+    return result;
   }
 
-  for (let i = 0; i < result.rows.length; i++) {
-    const lastName = peopleData.getLastName();
-    const firstName = peopleData.getFirstName();
-    const companyName = companyData.getCompanyName();
-    const email = companyData.getEmail(firstName, lastName, companyName);
-    const phone = peopleData.getPhone();
-    const amount = companyData.getAmount();
-
-    if (lastNameIndex !== -1) {
-      result.rows[i][fieldNames[lastNameIndex].name] = lastName;
-    }
-
-    if (firstNameIndex !== -1) {
-      result.rows[i][fieldNames[firstNameIndex].name] = firstName;
-    }
-
-    if (emailIndex !== -1) {
-      result.rows[i][fieldNames[emailIndex].name] = email;
-    }
-
-    if (phoneIndex !== -1) {
-      result.rows[i][fieldNames[phoneIndex].name] = phone;
-    }
-
-    if (fullNameIndex !== -1) {
-      result.rows[i][fieldNames[fullNameIndex].name] =
-        firstName + " " + lastName;
-    }
-
-    if (nameIndex !== -1) {
-      let name = firstName + " " + lastName;
-
-      if (entityName === "company") {
-        name = companyName;
-      }
-
-      if (entityName === "engagement") {
-        name = companyName;
-      }
-
-      result.rows[i][fieldNames[nameIndex].name] = name;
-    }
-
-    if (customerNameIndex !== -1) {
-      if (entityName === "project_role") {
-        result.rows[i][fieldNames[customerNameIndex].name] = companyName;
-      }
-    }    
-
-    if (amountFields.length > 0) {
-      for(let j = 0; j < amountFields.length; j++) {
-        result.rows[i][amountFields[j]] = amount;
-      }
-    }    
-    
-  }
-  
-  return result;
 };
 
 module.exports = {
--- src/test/test.js
+++ src/test/test.js
@@ -16,7 +16,7 @@ const promptHandler = require("../prompts/prompt_handler");
       let prompts = ["list all the active engagements", "list all sow files for alliance", "sort by found_files"];
       // prompts = ["list all the active engagements", "list all sow files for alliance", "help link", "sort by engagement name", "export to excel"];
       // prompts = ["help hr"];
-      prompts = ["list my prompts."] ;
+      prompts = ["sarasde."] ;
       // prompts = ["list all the recent records of prompt history"];
       
 
