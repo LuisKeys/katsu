0f4b6eb6645aaf8b04429b2dd25f86e9c8ff503a
LuisKeys <luis.paradela@gmail.com>
2024-07-19 14:57:54 -0300

    auth encrypt working

--- src/authentication/auth_user.ts
+++ src/authentication/auth_user.ts
@@ -1,14 +1,36 @@
 import { KatsuState } from '../db/katsu_db/katsu_state';
+import bcrypt from 'bcrypt';
 
+/**
+ * Authenticates a user by comparing the provided email and password with the users in the application state.
+ * @param email - The email of the user to authenticate.
+ * @param password - The password of the user to authenticate.
+ * @param state - The application state containing the list of users.
+ * @returns A boolean indicating whether the authentication was successful.
+ */
 const authUser = function (email: string, password: string, state: KatsuState): boolean {
+  const encPass = encryptPassword(password);
   for (const user of state.users) {
     if (user.email === email) {
-      return user.password === password;
+      return user.password === encPass;
     }
   }
+
   return false;
 }
 
+/**
+ * Encrypts a password using bcrypt.
+ * 
+ * @param password - The password to be encrypted.
+ * @returns The hashed password.
+ */
+const encryptPassword = function (password: string): string {
+  const key = process.env.AUTH_SALT as string;
+  const hashedPassword = bcrypt.hashSync(password, key);
 
+  return hashedPassword;
+};
 
-export { authUser };
+export { authUser, encryptPassword };
+
