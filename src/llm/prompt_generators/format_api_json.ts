df1c217e5875cc93eacdda3debbe9b36453bc1c2
LuisKeys <luis.paradela@gmail.com>
2024-08-26 22:53:16 -0300

    fixed json formatter

--- src/llm/prompt_generators/format_api_json.ts
+++ src/llm/prompt_generators/format_api_json.ts
@@ -1,6 +1,6 @@
 
 const createFormatjSONPrompt = (jSON: string): string => {
-  const llmPrompt = `Format the numbers, phones, amounts  and dates in a human readable way for the following jSON, return only the jSON with the correct formats. Remove unused fields from the 'fields' list and from each row in the 'rows' list:
+  const llmPrompt = `Format the numbers, phones, amounts  and dates in a human readable way for the following jSON, return only the jSON with the correct formats. Maintain the original format:
   ${jSON}`;
   return llmPrompt;
 }
